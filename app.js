2f593cd8fd5ee4aa23c426c951c73cf8737f7a97
Anton <antonlakin@gmail.com>
2013-09-20 23:01:16 -0700

    model works

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-var Aerogel = require('./index');
-
+var Aerogel = require('aerogel');
 var driver = new Aerogel.CrazyDriver();
 var copter = new Aerogel.Copter(driver);
 
@@ -16,104 +15,52 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.static(__dirname + '/public'))
 server.listen(3000);
+console.log('app listeneing')
 app.get('/', function(req, res) {
   res.render('home')
+  console.log('home sent')
 });
 
 
 var gSocket;
 io.sockets.on('connection', function(socket) {
-  console.log('conn')
+  console.log('socke connected')
   socket.on("click", click)
-  socket.on("move_to", setPos)
-  socket.on("thrust_target", setThrust)
-  gSocket = socket
 });
-
-function setPos(data) {
-  if (copter_state.ready) {
-    copter_state.roll_target = data.roll
-    copter_state.pitch_target = data.pitch
-  }
-}
 
 function click(data) {
   if (data.cmd === "start") return start()
   if (data.cmd === "stop") return stopCopter()
 }
 
-
-var throttle = 0;
-
-function setThrust(data) {
-  console.log('thrust')
-  copter_state.thrust_target = data.thrust_target * 100
-}
-
-
-
-var copter_state = {
-  roll_target: 0,
-  pitch_target: 0,
-  yaw_target: 0,
-  thrust_target: 0
-}:
-
-
-// setInterval(function() {
-//  // console.log(copter_state)
-// }, 1000)
-
-  function start() {
-    driver.findCopters().then(function(copters) {
-      if (copters.length === 0) {
-        console.error('No copters found! Is your copter turned on?');
-        process.exit(1);
-      }
-      if (copters.length === 1)
-        return copters[0];
-      if (optimist.argv.hasOwnProperty('c')) {
-        var patt = new RegExp('\/' + channel + '\/');
-        for (var i = 0; i < copters.length; i++) {
-          if (patt.test(copters[i]))
-            return copters[i];
-        }
-      }
+function start() {
+  driver.findCopters().then(function(copters) {
+    if (copters.length === 0) {
+      console.error('No copters found! Is your copter turned on?');
+      process.exit(1);
+    }
+    if (copters.length === 1)
       return copters[0];
-    }).then(function(uri) {
-      copter_state.ready = true;
-      console.log('Using copter at', uri);
-      return copter.connect(uri);
-    }).then(function() {
-      flyAround()
-      return
-    }).fail(function(err) {
-      console.log(err);
-      copter.shutdown();
-    })
-      .done();
-  }
-
-
-  function flyAround() {
-    copter_state.ready = true;
+    if (optimist.argv.hasOwnProperty('c')) {
+      var patt = new RegExp('\/' + channel + '\/');
+      for (var i = 0; i < copters.length; i++) {
+        if (patt.test(copters[i]))
+          return copters[i];
+      }
+    }
+    return copters[0];
+  }).then(function(uri) {
+    return copter.connect(uri);
+  }).then(function() {
     console.log('copter is ready')
-    orig = Date.now()
-    quad_synch = setInterval(function() {
-      copter.driver.setpoint(copter_state.roll_target, -copter_state.pitch_target, copter_state.yaw_target, copter_state.thrust_target)
-    }, 1000)
-  }
 
+    return
+  }).fail(function(err) {
+    console.log(err);
+    copter.shutdown();
+  })
+    .done();
+}
+//////
 
-
-  function stopCopter() {
-    copter.driver.setpoint(0, 0, 0, 0)
-    copter.shutdown()
-    console.log('copter shutdown')
-    clearInterval(quad_synch)
-  }
-
-setInterval(function() {
-  if (gSocket) gSocket.emit("telem", copter.telemetry.stabilizer)
-  console.log(copter.telemetry.stabilizer)
-}, 200)
+// new
--- public/javascripts/gen.js
+++ public/javascripts/gen.js
@@ -20,21 +20,36 @@ window.onload = function() {
       cmd: "stop"
     })
   })
-  document.querySelector(".data").textContent = "hi"
   window.onmousemove = handleMouseMove;
 
   function handleMouseMove(e) {
     if (start === true) {
 
       // console.log(e.x, e.y)
-      document.querySelector(".data").textContent = [(e.x - 200)/10, (e.y- 200)/10].join(", ")
+      document.querySelector(".data").textContent = [(e.x - 200) / 10, (e.y - 200) / 10].join(", ")
       document.querySelector(".me").style.left = Math.floor(e.x) + "px"
       document.querySelector(".me").style.top = Math.floor(e.y) + "px"
       socket.emit('move_to', {
-        roll: (e.x - 200)/10,
-        pitch: (e.y - 200)/10
+        roll: (e.x - 200) / 10,
+        pitch: (e.y - 200) / 10
       })
     }
+    window.requestAnimFrame = (function() {
+      return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function(callback) {
+          window.setTimeout(callback, 1000 / 60);
+      };
+    })();
+
+    // usage:
+    // instead of setInterval(render, 16) ....
+
+    (function animloop() {
+      requestAnimFrame(animloop);
+      animate();
+    })();
   }
 
   document.onkeydown = checkKey;
@@ -48,7 +63,7 @@ window.onload = function() {
       document.querySelector(".thrust_target").style.height = Math.floor(thrust_target) + "px"
     } else if (k.keyCode === 65) {
       thrust_target -= 20
-      if (thrust_target<0)  thrust_target =0
+      if (thrust_target < 0) thrust_target = 0
       document.querySelector(".thrust_target").style.height = Math.floor(thrust_target) + "px"
     }
 
@@ -57,6 +72,9 @@ window.onload = function() {
       thrust_target: thrust_target
     })
   }
+
+  init();
+  animate();
 }
 
 
@@ -70,6 +88,174 @@ function displayTelem(data) {
   document.querySelector(".copter").style.webkitTransform = rotate
   document.querySelector(".copter").style.left = (200 + Math.floor(data.roll)) + "px"
   document.querySelector(".copter").style.top = (200 + Math.floor(data.pitch)) + "px"
-  document.querySelector(".quad_thrust").style.height = Math.floor(data.thrust/100) + "px"
+  document.querySelector(".quad_thrust").style.height = Math.floor(data.thrust / 100) + "px"
+}
+
+
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+
+function init() {
+
+
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+  // camera = new THREE.Camera();
+  camera.position.x = -200;
+  camera.position.y = 100;
+  camera.position.z = 100;
+  scene = new THREE.Scene();
+
+
+  // set up coordinates
+  coordinates_group = new THREE.Object3D();
+  var line_mat = new THREE.LineBasicMaterial({
+    color: 0x6699FF,
+    linewidth: 2,
+    fog: false
+  })
+  // set up x axis
+  var x_axis_geometry = new THREE.Geometry();
+  x_axis_geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  x_axis_geometry.vertices.push(new THREE.Vector3(500, 0, 0));
+  x_axis = new THREE.Line(x_axis_geometry, line_mat)
+  coordinates_group.add(x_axis)
+  // set up y axis
+  var y_axis_geometry = new THREE.Geometry();
+  y_axis_geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  y_axis_geometry.vertices.push(new THREE.Vector3(0, 500, 0));
+  y_axis = new THREE.Line(y_axis_geometry, line_mat)
+  coordinates_group.add(y_axis)
+  // set up z axis
+  var z_axis_geometry = new THREE.Geometry();
+  z_axis_geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  z_axis_geometry.vertices.push(new THREE.Vector3(0, 0, 500));
+  z_axis = new THREE.Line(z_axis_geometry, line_mat)
+  coordinates_group.add(z_axis)
+
+  scene.add(coordinates_group)
+  // end of axis setup
+
+  // set up quad model / by arm
+  quad_group = new THREE.Object3D();
+  var front_mat = new THREE.MeshBasicMaterial({
+    color: "blue",
+    wireframe: false
+  });
+  var side_mat = new THREE.MeshBasicMaterial({
+    color: "black",
+    wireframe: false
+  });  
+  var back_mat = new THREE.MeshBasicMaterial({
+    color: "red",
+    wireframe: false
+  });
+
+  var arm_length = 100;
+  var arm_height = 2;
+  var arm_width = 10;
+
+  var pi=  Math.PI
+  // m1 arm
+
+
+  m1 = new THREE.CubeGeometry(arm_length, arm_height, arm_width)
+  m1.applyMatrix(new THREE.Matrix4().makeTranslation(arm_length / 2, arm_height / 2, 0));
+
+
+  m1_mesh = new THREE.Mesh(m1, front_mat);
+  m1_mesh.rotation.y = 0
+  // m2 arm
+  m2_mesh = new THREE.Mesh(m1, side_mat);
+  m2_mesh.rotation.y = -pi/2
+  // m3 arm
+  m3_mesh = new THREE.Mesh(m1, back_mat);
+  m3_mesh.rotation.y = pi
+  // m4 arm
+  m4_mesh = new THREE.Mesh(m1, side_mat);
+  m4_mesh.rotation.y = pi/2
+
+
+  var quad_mat = new THREE.MeshBasicMaterial({
+    color: "orange",
+    wireframe: false
+  });
+  var v_length = 2;
+  var v_height = 10;
+  var v_width = 2;
+  var offset = v_length/2
+
+  
+  v1 = new THREE.CubeGeometry(v_length, v_height, v_width)
+  v1.applyMatrix(new THREE.Matrix4().makeTranslation(0, (v_height / 2), 0));
+  // m1 motor
+
+
+  v1_mesh = new THREE.Mesh(v1, quad_mat);
+  v1_mesh.position.x = arm_length-offset;
+  v1_mesh.position.y = arm_height;
+
+  // m2 motor
+  v2_mesh = new THREE.Mesh(v1, quad_mat);
+  v2_mesh.position.z = arm_length-offset;
+  v2_mesh.position.y = arm_height;
+  // m3
+  v3_mesh = new THREE.Mesh(v1, quad_mat);
+  v3_mesh.position.x = -arm_length+offset;
+  v3_mesh.position.y = arm_height;
+  // m4
+  v4_mesh = new THREE.Mesh(v1, quad_mat);
+  v4_mesh.position.z = -arm_length+offset;
+  v4_mesh.position.y = arm_height;
+
+
+
+  quad_group.add(m1_mesh)
+  quad_group.add(v1_mesh)
+  quad_group.add(m2_mesh)
+  quad_group.add(v2_mesh)
+  quad_group.add(m3_mesh)
+  quad_group.add(v3_mesh)
+  quad_group.add(m4_mesh)
+  quad_group.add(v4_mesh)
+  quad_group.position.x = 0
+  quad_group.position.y = 0
+  quad_group.position.z = 0
+
+  scene.add(quad_group);
+
+
+  camera.lookAt(quad_group.position)
+  renderer = new THREE.CanvasRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+}
+
+function animate() {
+
+  // note: three.js includes requestAnimationFrame shim
+  requestAnimationFrame(animate);
+  // coordinates_group.rotation.y += 0.0001;
+  // coordinates_group.rotation.z += 0.0001;
+  quad_group.rotation.y += .009;
+
+  renderer.render(scene, camera);
+
+function animate() {
+
+  // note: three.js includes requestAnimationFrame shim
+  requestAnimationFrame(animate);
+  // coordinates_group.rotation.y += 0.0001;
+  // coordinates_group.rotation.z += 0.0001;
+  quad_group.rotation.y += .0001;
+
+  renderer.render(scene, camera);
 
 }
+
+
+// shim layer with setTimeout fallback
+
+// place the rAF *before* the render() to assure as close to
+// 60fps with the setTimeout fallback.
