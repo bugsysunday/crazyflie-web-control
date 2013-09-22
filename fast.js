e853c36e0782d61d089ac651ba3e196468a2d597
Anton <antonlakin@gmail.com>
2013-09-21 21:39:29 -0700

    add order of commands

--- fast.js
+++ fast.js
@@ -2,30 +2,35 @@ var Aerogel = require('aerogel');
 var driver = new Aerogel.CrazyDriver();
 var copter = new Aerogel.Copter(driver);
 var tb = require('toobusy')
+var optimist = require('optimist')
 
 
   function start() {
     driver.findCopters().then(function(copters) {
-      if (copters.length === 0) {
-        console.error('No copters found! Is your copter turned on?');
-        // process.exit(1);
-      }
-      if (copters.length === 1)
-        return copters[0];
-      if (optimist.argv.hasOwnProperty('c')) {
-        var patt = new RegExp('\/' + channel + '\/');
-        for (var i = 0; i < copters.length; i++) {
-          if (patt.test(copters[i]))
-            return copters[i];
-        }
-      }
-      return copters[0];
+      // console.log(copters)
+      // if (copters.length === 0) {
+      //   console.error('No copters found! Is your copter turned on?');
+      //   // process.exit(1);
+      // }
+      // if (copters.length === 1)
+      //   return copters[0];
+      // if (optimist.argv.hasOwnProperty('c')) {
+      //   var patt = new RegExp('\/' + channel + '\/');
+      //   for (var i = 0; i < copters.length; i++) {
+      //     if (patt.test(copters[i]))
+      //       console.log("a copter", copters[i])
+      //       return copters[i];
+      //   }
+      // }
+      return "radio://1/10/250KPS";
     }).then(function(uri) {
-      return copter.connect(uri);
+      console.log('uri 0------------->', uri)
+      return copter.connect("radio://1/10/250KPS");
     }).then(function() {
       console.log('copter is ready')
       console.log('lag is ', tb.lag())
-      fly()
+      pullNext()
+      startControl()
       return
     }).fail(function(err) {
       console.log(err);
@@ -42,21 +47,39 @@ var quad_state = {
   target_date: 0
 };
 
+var commands = [{
+  action: fly,
+  period: 1000
+}, {
+  action: land,
+  period: 1000
+}]
 
-function fly() {
-  console.log('fly')
-  quad_state.target_throttle = 50000;
-  quad_state.target_throttle_time = Date.now() + 2000;
-  startControl()
-}
 
-function land() {
-  console.log('land')
-  quad_state.target_throttle = 0;
-  quad_state.target_throttle_time = Date.now() + 3000;
-  startControl()
-}
+  function pullNext() {
+    var current = commands.shift() 
+    if (!current) process.exit();
 
+    current.action(current.period)
+  }
+
+  function fly(ms) {
+    console.log('fly!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    quad_state.target_throttle = 20000;
+    quad_state.target_throttle_time = Date.now() + ms;
+  }
+
+  function land(ms) {
+    console.log('land!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    quad_state.target_throttle = 10000;
+    quad_state.target_throttle_time = Date.now() + ms;
+    startControl()
+  }
+
+
+  function hover() {
+
+  }
 
 var control;
 var send_every = 50;
@@ -75,7 +98,7 @@ function startControl() {
         quad_state.last_set_throttle = quad_state.target_throttle
         copter.driver.setpoint(0, 0, 0, quad_state.last_set_throttle)
         console.log('set to ', quad_state.last_set_throttle)
-        setTimeout(land, 100)
+        pullNext()
       } else {
         console.log('time to target', time_to_target)
         var step = (quad_state.target_throttle - quad_state.last_set_throttle) / time_to_target; // throt per minute
@@ -87,7 +110,7 @@ function startControl() {
         var set_value = Math.floor(ms_since_last * step)
         copter.driver.setpoint(0, 0, 0, quad_state.last_set_throttle)
         last_update = now
-        quad_state.last_set_throttle +=set_value
+        quad_state.last_set_throttle += set_value
       }
 
       // console.log('loop took', Date.now() - now)
